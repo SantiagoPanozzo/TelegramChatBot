39e01cd221c092b61328d75081a0ed01efa0611f
Tomás Esteves <estevestomas27@gmail.com>
2022-10-29 18:05:01 -0300

    agora sim

--- src/Library/Buscador.cs
+++ src/Library/Buscador.cs
@@ -10,10 +10,14 @@ public class Buscador {
         return of;
     }
 
+    /* Como empleador, quiero ver el resultado de las búsquedas de ofertas de trabajo ordenado en forma ascendente de distancia a mi ubicación, 
+    es decir, las más cercanas primero para que de esa forma, pueda poder contratar un servicio. */
     public void FiltrarDistancia(Empleador emp) {
 
     }
 
+    /* Como empleador, quiero ver el resultado de las búsquedas de ofertas de trabajo ordenado en forma descendente por reputación, 
+    es decir, las de mejor reputación primero para que de esa forma, pueda contratar un servicio. */
     public void FiltrarReputacion() {
         
     }
