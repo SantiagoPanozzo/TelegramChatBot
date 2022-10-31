7eb276f1f610eee09b6298397ad3044c7c21fd55
SnowieMischief <97817883+SnowieMischief@users.noreply.github.com>
2022-10-31 00:25:21 -0300

    Update Buscador.cs

--- src/Library/Buscador.cs
+++ src/Library/Buscador.cs
@@ -21,16 +21,50 @@ public class Buscador {
         OfertasHandler handler = OfertasHandler.GetInstance();
         List<Categoria> categorias = handler.GetCategorias();
 
-        var myPos = emp.Ubicacion;
-
-        foreach(Categoria categoria in categorias)
+        Tuple<double,double> myPos = emp.Ubicacion;
+        foreach(Categoria cat in categorias)
         {
-            var categoryId = categoria.GetId();
-            offers.AddRange(handler.GetOfertas(categoryId));
+            foreach(OfertaDeServicio oferta in cat.GetOfertas()){
+                offers.Add(oferta);}
         }
 
+        OfertaDeServicio shortest;
+        int ofertas = offers.Count;
+        for(int k = 0; k < ofertas; k ++){
+            shortest = GetShortest(myPos, offers);
+            resultOffers.Add(shortest);
+            offers.Remove(shortest);
+        }
         return resultOffers;
     }
+
+    private OfertaDeServicio? GetShortest(Tuple<double,double> myPos, List<OfertaDeServicio> lista)
+    {
+        int i = 0;
+        double distanciaActual = 0;
+        double distanciaMenor = double.MaxValue;
+        OfertaDeServicio? shortest = null;
+        foreach(OfertaDeServicio oferta in lista)
+        {
+            i++;               
+            double latO = oferta.GetUbicacion().Item1;
+            double longO = oferta.GetUbicacion().Item2;
+            double latE = myPos.Item1;
+            double longE = myPos.Item2;
+            double term1 = Math.Pow((latE - latO), 2);
+            double term2 = Math.Pow((longE - longO), 2);
+            distanciaActual = Math.Sqrt(term1+term2);
+
+            if (i <= 1) continue;
+            if (distanciaMenor > distanciaActual)
+            {
+                distanciaMenor = distanciaActual;
+                shortest = oferta;
+            }
+        }
+        return shortest;
+    }
+
 
     /* Como empleador, quiero ver el resultado de las búsquedas de ofertas de trabajo ordenado en forma
     descendente por reputación, es decir, las de mejor reputación primero para que de esa forma, pueda
