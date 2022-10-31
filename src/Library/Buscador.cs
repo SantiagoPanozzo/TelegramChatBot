9d5f4f533d6fd9bc5dbc990d0a8bdfd1d94ab044
Facundo Martínez <facusml16@gmail.com>
2022-10-30 23:59:25 -0300

    Cambios

--- src/Library/Buscador.cs
+++ src/Library/Buscador.cs
@@ -15,9 +15,22 @@ public class Buscador {
     /* Como empleador, quiero ver el resultado de las búsquedas de ofertas de trabajo ordenado en forma
     ascendente de distancia a mi ubicación, es decir, las más cercanas primero para que de esa forma,
     pueda poder contratar un servicio. */
-    //public int FiltrarDistancia(Empleador emp) {      TODO: Emplear con API, cambiar clases para este propósito
+   /* public List<OfertaDeServicio> FiltrarDistancia(Empleador emp) {      //TODO: Emplear con API, cambiar clases para este propósito
+        List<OfertaDeServicio> offers = new();
+        List<OfertaDeServicio> resultOffers = new();
+        OfertasHandler handler = OfertasHandler.GetInstance();
+        List<Categoria> categorias = handler.GetCategorias();
 
-    //}
+        var myPos = emp.Ubicacion;
+
+        foreach(Categoria categoria in categorias)
+        {
+            var categoryId = categoria.GetId();
+            offers.AddRange(handler.GetOfertas(categoryId));
+        }
+
+        return resultOffers;
+    }*/
 
     /* Como empleador, quiero ver el resultado de las búsquedas de ofertas de trabajo ordenado en forma
     descendente por reputación, es decir, las de mejor reputación primero para que de esa forma, pueda
@@ -43,7 +56,7 @@ public class Buscador {
             {
                 if(offer.GetReputacion() == (Calificacion)j)
                 {
-                    resultOffers.Prepend(offer);
+                    resultOffers.Insert(0, offer);
                 }
             }
         }
