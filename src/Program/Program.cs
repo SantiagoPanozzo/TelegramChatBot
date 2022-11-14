f98af699926e4baaee121ea82db63aa4f0b99e2a
Tomás Esteves <estevestomas27@gmail.com>
2022-11-14 17:30:23 -0300

    algo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,54 +1,158 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
 using Library;
 using Library.DistanceMatrix;
 using System;
 
-class Program {
+public class Program {
+    // La instancia del bot.
+    private static TelegramBotClient Bot;
+
+    // El token provisto por Telegram al crear el bot. Mira el archivo README.md en la raíz de este repo para
+    // obtener indicaciones sobre cómo configurarlo.
+    private static string token;
+
+    // Esta clase es un POCO -vean https://en.wikipedia.org/wiki/Plain_old_CLR_object- para representar el token
+    // secreto del bot.
+    private class BotSecret {
+        public string Token { get; set; }
+    }
+
+    // Una interfaz requerida para configurar el servicio que lee el token secreto del bot.
+    private interface ISecretService {
+        string Token { get; }
+    }
+
+    // Una clase que provee el servicio de leer el token secreto del bot.
+    private class SecretService : ISecretService {
+        private readonly BotSecret _secrets;
+
+        public SecretService(IOptions<BotSecret> secrets) {
+            _secrets = secrets.Value ?? throw new ArgumentNullException(nameof(secrets));
+        }
+
+        public string Token { get { return _secrets.Token; } }
+    }
+
+    // Configura la aplicación.
+    private static void Start() {
+        // Lee una variable de entorno NETCORE_ENVIRONMENT que si no existe o tiene el valor 'development' indica
+        // que estamos en un ambiente de desarrollo.
+        var developmentEnvironment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+        var isDevelopment =
+            string.IsNullOrEmpty(developmentEnvironment) ||
+            developmentEnvironment.ToLower() == "development";
+
+        var builder = new ConfigurationBuilder();
+        builder
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        // En el ambiente de desarrollo el token secreto del bot se toma de la configuración secreta
+        if (isDevelopment) {
+            builder.AddUserSecrets<Program>();
+        }
+
+        var configuration = builder.Build();
+
+        IServiceCollection services = new ServiceCollection();
+
+        // Mapeamos la implementación de las clases para inyección de dependencias
+        services
+            .Configure<BotSecret>(configuration.GetSection(nameof(BotSecret)))
+            .AddSingleton<ISecretService, SecretService>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var revealer = serviceProvider.GetService<ISecretService>();
+        token = revealer.Token;
+    }
+    
+    private static IHandler firstHandler;
+
     public static void Main() {
+        Start();
 
-        //Console.WriteLine(Distance.Calculate("Salto Uruguay", "Montevideo Uruguay"));
-        /*
-        Administrador admin = new("sd", "sd", "9231015", "sfa@sda.com");
-        OfertasHandler handler = OfertasHandler.GetInstance();
-        handler.CrearCategoria(admin, "a");
-        handler.CrearCategoria(admin, "b");
-         
-        CategoriaPrinter catPrinter = new();
-        List<Categoria> a = new();
-
-        a.Add(c1);
-        a.Add(c2);
-        a.Add(c3);
-
-        catPrinter.PrintCatalog(a);
-        Administrador a1 = new("nick", "contra", "tel", "a@b.c");
-
-        //OfertaDeServicioPrinter ofePrinter = new();
-        ContratoHandler ch = new(); 
-
-
-        Trabajador t1 = new("Ihojan", "Werlyb", "hide on bush", "1234", new DateTime(2020,2,1), "11111111", "099", "a@b.c", new Tuple<double, double>(-31.389425985682045, -57.959432913914476));
-        Trabajador t2 = new("Cosplay de", "Irelia", "hide on bush", "1234", new DateTime(2020,2,1), "11111111", "099", "a@b.c", new Tuple<double, double>(-31.389425985682045, -57.959432913914476));
-
-        Empleador e1 = new("Paquito", "Paco", "hide on bush", "1234", new DateTime(2020,2,1), "11111111", "099", "a@b.c", new Tuple<double, double>(-31.389425985682045, -57.959432913914476));
-        Empleador e2 = new("Sigo?", "Cont", "hide on bush", "1234", new DateTime(2020,2,1), "11111111", "099", "a@b.c", new Tuple<double, double>(-31.389425985682045, -57.959432913914476));
-
-        handler.Ofertar(1, t1, "asd", "asf", 24.6);
-        handler.Ofertar(2, t2, "asd", "asf", 46);
-        handler.Ofertar(1, t2, "sfa", "wqr", 99);
-        Solicitud s1 = new(o1, e1);
-        Solicitud s2 = new(o2, e2);
-
-        ch.Catalogo.AddSolicitud(o1, e1);
-        ch.Catalogo.AddSolicitud(o2, e2);
-
-        o1.DarDeBaja(a1);
-
-        ofePrinter.PrintCatalog(ch.Catalogo);
+        Bot = new TelegramBotClient(token);
         
-        */
+        firstHandler = new InfoHandler(new CategoriasHandler(Bot, null));
+
+        var cts = new CancellationTokenSource();
+
+        // Comenzamos a escuchar mensajes. Esto se hace en otro hilo (en background). El primer método
+        // HandleUpdateAsync es invocado por el bot cuando se recibe un mensaje. El segundo método HandleErrorAsync
+        // es invocado cuando ocurre un error.
+        Bot.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            new ReceiverOptions()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            },
+            cts.Token
+        );
+
+        Console.WriteLine($"Bot listo y funcionando");
+
+        // Esperamos a que el usuario aprete Enter en la consola para terminar el bot.
+        Console.ReadLine();
+
+        // Terminamos el bot.
+        cts.Cancel();
+    }
+
+    /// <summary>
+    /// Maneja las actualizaciones del bot (todo lo que llega), incluyendo mensajes, ediciones de mensajes,
+    /// respuestas a botones, etc. En este ejemplo sólo manejamos mensajes de texto.
+    /// </summary>
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) {
+        try {
+            // Sólo respondemos a mensajes de texto
+            if (update.Type == UpdateType.Message) {
+                await HandleMessageReceived(botClient, update.Message);
+            }
+        }
+        catch(Exception e) {
+            await HandleErrorAsync(botClient, e, cancellationToken);
+        }
+    }
+
+    /// <summary>
+    /// Maneja los mensajes que se envían al bot a través de handlers de una chain of responsibility.
+    /// </summary>
+    /// <param name="message">El mensaje recibido</param>
+    /// <returns></returns>
+    private static async Task HandleMessageReceived(ITelegramBotClient botClient, Message message) {
+        Console.WriteLine($"Received a message from {message.From.FirstName} saying: {message.Text}");
+
+        string response = string.Empty;
+
+        firstHandler.Handle(message, out response);
+
+        if (!string.IsNullOrEmpty(response)) {
+            await Bot.SendTextMessageAsync(message.Chat.Id, response);
+        }
+    }
+
+    /// <summary>
+    /// Manejo de excepciones. Por ahora simplemente la imprimimos en la consola.
+    /// </summary>
+    public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken) {
+        Console.WriteLine(exception.Message);
+        return Task.CompletedTask;
     }
 }
