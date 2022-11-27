9122630fd61aaf75d5063aed4e5115fb179f1bea
Facundo Martínez <facusml16@gmail.com>
2022-11-27 20:40:25 -0300

    Nuevos printers e interfaz

--- src/Library/Printers/IConsolePrinter.cs
+++ src/Library/Printers/IConsolePrinter.cs
@@ -2,6 +2,6 @@ namespace Library;
 using System;
 
 /// <summary> Interfáz para mostrar datos por pantalla </summary>
-public interface IPrinter<T> {
-    void PrintCatalog(List<T> catalog, Usuario user);
+public interface IPrinter<T, U>{
+    U Print(List<T> catalog, Usuario user);
 }
--- src/Library/Printers/SolicitudPrinter.cs
+++ src/Library/Printers/SolicitudPrinter.cs
@@ -1,12 +1,12 @@
 namespace Library;
 
 /// <summary>Método para mostrar por pantalla la lista de solicitudes</summary>
-public class SolicitudPrinter : IPrinter<Solicitud> {
+public class SolicitudPrinter : IConsolePrinter<Solicitud> {
 
     /// <summary> Método que imprime el texto de la lista </summary>
     /// <param name="solicitudes"> Lista de solicitudes </param>
     /// <param name="user"> Tipo de usuario que llama al método </param>
-    public void PrintCatalog(List<Solicitud> solicitudes, Usuario user) {
+    public void Print(List<Solicitud> solicitudes, Usuario user) {
         Console.WriteLine($"Solicitudes\n");
         if (user.GetTipo().Equals(TipoDeUsuario.Empleador)){
             foreach (var sol in solicitudes) {
